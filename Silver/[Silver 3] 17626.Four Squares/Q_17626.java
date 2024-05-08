e273ce7b9d7f93d77d465e61049728fdd8fe602b
HuiSeokJeong <162408580+hs12137@users.noreply.github.com>
2024-05-08 23:52:46 +0900

    2024-05-08 q.17626해결

---
+++ Silver/[Silver 3] 17626.Four Squares/Q_17626.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_17626 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        int min;
+        for (int i = 2; i <= n; i++) {
+            min = 50001;
+
+            for (int j = 1; j * j <= i; j++) {
+                int tmp = i - (j * j);
+                min = Math.min(min, dp[tmp]);
+            }
+            dp[i] = min + 1;
+        }
+
+        System.out.print(dp[n]);
+    }
+}
